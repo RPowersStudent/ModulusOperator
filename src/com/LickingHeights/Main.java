de5ad9c67be2e20156af5d69a6e5db4ff490c6ab
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-02-28 14:17:15 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,21 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+    }
+    public static void testOddness() {
+        int number;
+        Scanner keyboard ;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("type a positive number");
+        number = keyboard.nextInt();
+
+        System.out.println("Number is even: "+(number %2 ==0));
+        System.out.println("Number is odd: " +(number %2 != 0));
     }
 }
