d9981de8b7e1ea2ebec333c01ce7f7881f61e94a
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-03-02 14:11:25 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         testOddness();
         keepContained();
+        lastDigit();
     }
     public static void testOddness() {
         int number;
@@ -28,6 +29,17 @@ public class Main {
         number =keyboard.nextInt();
 
         System.out.println("The number contained is:"+((number%8)+5));
+
+    }
+    public static void lastDigit(){
+        int number;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("please type a number that is four places long");
+        number = keyboard.nextInt();
+
+        System.out.println("the last digit of the number is: "+ number%10);
 
     }
 }
