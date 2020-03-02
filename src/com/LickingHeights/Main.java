13f56ef3bd0bc4fe4e86707450b14096c439549c
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-03-02 13:59:33 -0500

    keepContainedMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         testOddness();
+        keepContained();
     }
     public static void testOddness() {
         int number;
-        Scanner keyboard ;
+        Scanner keyboard;
         keyboard = new Scanner(System.in);
 
         System.out.println("type a positive number");
@@ -17,5 +18,16 @@ public class Main {
 
         System.out.println("Number is even: "+(number %2 ==0));
         System.out.println("Number is odd: " +(number %2 != 0));
+    }
+    public static void keepContained(){
+        int number;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out. println("please type a positive number");
+        number =keyboard.nextInt();
+
+        System.out.println("The number contained is:"+((number%8)+5));
+
     }
 }
